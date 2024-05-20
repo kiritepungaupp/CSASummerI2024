a871415f21c735167c39b275658cf61ff753a74c
kiritepungaupp <156395646+kiritepungaupp@users.noreply.github.com>
2024-05-20 14:33:52 +0700

    ok

--- githublab1.py
+++ githublab1.py
@@ -1,6 +1,5 @@
+#minute = eval(input("Please input minutes: "))
 
+print(f'{} minutes is aproximately {(//525600)} and {(525600)//1440} days'.format((minute := input())))
 
-minute = eval(input("Please input minutes: "))
-
-print(f'{minute} minutes is aproximately {(minute//525600)} and {minute%(525600)//1440} days')
 
